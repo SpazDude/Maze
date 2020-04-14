b6d51d01f890892d7d6197208999b264155e0fb9
SpazDude <aaron@incwells.com>
2020-04-13 21:31:04 -0700

    create image

---
+++ MazeLibrary/Node.cs
@@ -0,0 +1,11 @@
+﻿namespace MazeLibrary
+{
+
+
+    public class Node
+    {
+        public int x, y;
+        public int group;
+        public bool left = true, top = true, right = true, bottom = true;
+    }
+}
